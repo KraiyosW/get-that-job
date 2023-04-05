5c71e35fffbe86cc8e4c0f5b4b0dab5afbfacf2c
YokYok <patiparn.wong@gmail.com>
2023-04-06 00:37:40 +0700

    feat: add Navbar and Content section

---
+++ src/components/Contentsection.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import findjob from '../image/icon.svg'
+import Image from 'next/image'
+
+const Contentsection = () => {
+  return (
+
+    <div className="w-full h-screen flex flex-row mt-[20px]">
+        <div className="w-7/12 h-3/5 bg-[#BF5F82] flex flex-row justify-center items-center ">
+          <div className="w-9/12 h-auto flex flex-col">
+            <h1 className="text-7xl mb-5 text-white">Find your next job</h1>
+            <p className="text-3xl my-5 text-white">
+              Our Machine learning algorithm is so good that it's even illegal
+              in some countries. Join us to use our barelly legal algorithm that
+              is actually a group of interns that work on our basement.
+            </p>
+            <p className="text-3xl my-5 text-white">
+              We have a job for you, no matter your background or previous
+              experience. Is sending random memes through chat your only skill?
+              That's ok, we got you, our Rock Star Meme Curator role is here for
+              you.
+            </p>
+          </div>
+        </div>
+        <div className="w-5/12 h-3/5 flex my-[40] bg-white justify-center items-center">
+          <Image
+            alt="find-your-next-job-icon"
+            src={findjob}
+            className="w-[60%] h-[51%]"
+          />
+        </div>
+      </div>
+
+  )
+}
+
+export default Contentsection
