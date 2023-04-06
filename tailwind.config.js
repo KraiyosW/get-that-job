563c0ebf8a7d45e477b8d046901134452e41e77f
KraiyosW <kraiyos.wanna@gmail.com>
2023-04-06 15:15:50 +0700

    fix: tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,13 +9,7 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      width :{
-        '720' :'45rem',
-        '128': '32rem',
-        '416': '26rem'
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
