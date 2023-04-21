b3e0732220c59848fb6e5655dc81c737596f93f6
Bright <22whiskey.smd@gmail.com>
2023-04-21 12:26:13 +0700

    feat:add some css and uncomment auth

--- src/components/SidebarRecruiter.js
+++ src/components/SidebarRecruiter.js
@@ -13,10 +13,10 @@ import { useRouter } from "next/router";
 
 const SideBarRecruiter = () => {
 
-  const {isAuthenticated ,logoutAuth} = useAuth();
+  const { isAuthenticated, logoutAuth } = useAuth();
   const router = useRouter();
 
-  if(!isAuthenticated){
+  if (!isAuthenticated) {
     return null;
   }
 
@@ -30,65 +30,65 @@ const SideBarRecruiter = () => {
     }
   };
 
- 
+
   return (
     <div className="m-0 p-0 w-[240px] bg-white-tertiary fixed h-screen overflow-auto max-[700px]:w-screen max-[700px]:h-auto max-[700px]:relative" id="sidebar">
-    <Link href="/" className="max-[700px]:flex max-[700px]:justify-center">
-      <Image src={GTJ} className="mb-4 max-[700px]:pl-[8px] max-[700px]:pt-[16px] pl-[16px] pt-[32px] w-[160px] cursor-pointer max-[700px]:hidden" />
-    </Link>
-    <div className="box-all">
-    <div className="box-1 max-[700px]:h-auto h-[82vh] max-[700px]:flex max-[700px]:flex-row max-[700px]:w-full max-[700px]:justify-between">
-    <Link href="/" className="max-[700px]:flex max-[700px]:justify-center">
-      <div className="hover:bg-white-secondary active:bg-white-secondary max-[700px]:p-[6px] p-[12px] flex flex-row flex-warp gap-[10px]" id="sidebar-menu">
+      <Link href="/" className="max-[700px]:flex max-[700px]:justify-center">
+        <Image src={GTJ} className="mb-4 max-[700px]:pl-[8px] max-[700px]:pt-[16px] pl-[16px] pt-[32px] w-[160px] cursor-pointer max-[700px]:hidden" />
+      </Link>
+      <div className="box-all">
+        <div className="box-1 max-[700px]:h-auto h-[82vh] max-[700px]:flex max-[700px]:flex-row max-[700px]:w-full max-[700px]:justify-between">
+          <Link href="/jobPostings" className="max-[700px]:flex max-[700px]:justify-center">
+            <div className="hover:bg-white-secondary active:bg-white-secondary max-[700px]:p-[6px] p-[12px] flex flex-row flex-warp gap-[10px]" id="sidebar-menu">
 
-            <Image src={bag} alt="Job Postings" className="max-[700px]:hidden" />
-            <p className="max-[700px]:text-[10px] max-[700px]:leading-[17px] max-[700px]:w-[70px] max-[700px]:text-center max-[700px]:flex max-[700px]:justify-center max-[700px]:items-center text-[1rem] leading-[1.5rem] font-normal">Job Postings</p>
-        </div>
-        <div className="py-[8px]">
-        <Image src={Line} alt="Line" className="min-[701px]:hidden h-full w-[3px] rounded-[10px]" />
-        </div>
-        </Link>
-
-          <Link href="/" className="max-[700px]:flex max-[700px]:justify-center">
-        <div className="hover:bg-white-secondary active:bg-white-secondary max-[700px]:p-[6px] p-[12px] flex flex-row flex-warp gap-[10px]" id="sidebar-menu">
-            <Image src={paper} alt="Create New Job" className="max-[700px]:hidden" />
-            <p className="max-[700px]:text-[10px] max-[700px]:leading-[17px] max-[700px]:w-[70px] max-[700px]:text-center max-[700px]:flex max-[700px]:justify-center max-[700px]:items-center text-[1rem] leading-[1.5rem] font-normal">Create New Job</p>
-        </div>
-        <div className="py-[8px]">
-        <Image src={Line} alt="Line" className="min-[701px]:hidden h-full w-[3px] rounded-[10px]" />
-        </div>
+              <Image src={bag} alt="Job Postings" className="max-[700px]:hidden" />
+              <p className="max-[700px]:text-[10px] max-[700px]:leading-[17px] max-[700px]:w-[70px] max-[700px]:text-center max-[700px]:flex max-[700px]:justify-center max-[700px]:items-center text-[1rem] leading-[1.5rem] font-normal">Job Postings</p>
+            </div>
+            <div className="py-[8px]">
+              <Image src={Line} alt="Line" className="min-[701px]:hidden h-full w-[3px] rounded-[10px]" />
+            </div>
           </Link>
 
-          <Link href="/" className="max-[700px]:flex max-[700px]:justify-center">
-        <div className="hover:bg-white-secondary active:bg-white-secondary max-[700px]:p-[6px] p-[12px] flex flex-row flex-warp gap-[7px]" id="sidebar-menu">
-            <Image src={edit} alt="Create New Job" className="ml-[4px] max-[700px]:hidden" />
-            <p className="max-[700px]:text-[10px] max-[700px]:leading-[17px] max-[700px]:w-[70px] max-[700px]:text-center max-[700px]:flex max-[700px]:justify-center max-[700px]:items-center text-[1rem] leading-[1.5rem] font-normal">Edit Job</p>
-        </div>
-        <div className="py-[8px]">
-        <Image src={Line} alt="Line" className="min-[701px]:hidden h-full w-[3px] rounded-[10px]" />
-        </div>
+          <Link href="/createNewJob" className="max-[700px]:flex max-[700px]:justify-center">
+            <div className="hover:bg-white-secondary active:bg-white-secondary max-[700px]:p-[6px] p-[12px] flex flex-row flex-warp gap-[10px]" id="sidebar-menu">
+              <Image src={paper} alt="Create New Job" className="max-[700px]:hidden" />
+              <p className="max-[700px]:text-[10px] max-[700px]:leading-[17px] max-[700px]:w-[70px] max-[700px]:text-center max-[700px]:flex max-[700px]:justify-center max-[700px]:items-center text-[1rem] leading-[1.5rem] font-normal">Create New Job</p>
+            </div>
+            <div className="py-[8px]">
+              <Image src={Line} alt="Line" className="min-[701px]:hidden h-full w-[3px] rounded-[10px]" />
+            </div>
           </Link>
 
-          <Link href="/" className="max-[700px]:flex max-[700px]:justify-center">
-        <div className="hover:bg-white-secondary active:bg-white-secondary max-[700px]:p-[6px] p-[12px] flex flex-row flex-warp gap-[10px]" id="sidebar-menu">
-            <Image src={profile} alt="Profile" className="max-[700px]:hidden" />
-            <p className="max-[700px]:text-[10px] max-[700px]:leading-[17px] max-[700px]:w-[70px] max-[700px]:text-center max-[700px]:flex max-[700px]:justify-center max-[700px]:items-center text-[1rem] leading-[1.5rem] font-normal">Profile</p>
-        </div>
-        <div className="py-[8px]">
-        <Image src={Line} alt="Line" className="min-[701px]:hidden h-full w-[3px] rounded-[10px]" />
-        </div>
+          <Link href="/editJobPosting" className="max-[700px]:flex max-[700px]:justify-center">
+            <div className="hover:bg-white-secondary active:bg-white-secondary max-[700px]:p-[6px] p-[12px] flex flex-row flex-warp gap-[7px]" id="sidebar-menu">
+              <Image src={edit} alt="Edit Job" className="ml-[4px] max-[700px]:hidden" />
+              <p className="max-[700px]:text-[10px] max-[700px]:leading-[17px] max-[700px]:w-[70px] max-[700px]:text-center max-[700px]:flex max-[700px]:justify-center max-[700px]:items-center text-[1rem] leading-[1.5rem] font-normal">Edit Job</p>
+            </div>
+            <div className="py-[8px]">
+              <Image src={Line} alt="Line" className="min-[701px]:hidden h-full w-[3px] rounded-[10px]" />
+            </div>
           </Link>
 
-          <Link onClick={handleLogout} href="/" className="max-[700px]:flex max-[700px]:justify-center">
-        <div className="hover:bg-white-secondary active:bg-white-secondary max-[700px]:p-[6px] p-[12px] flex flex-row flex-warp gap-[10px]" id="sidebar-menu">
+          <Link href="/profile" className="max-[700px]:flex max-[700px]:justify-center">
+            <div className="hover:bg-white-secondary active:bg-white-secondary max-[700px]:p-[6px] p-[12px] flex flex-row flex-warp gap-[10px]" id="sidebar-menu">
+              <Image src={profile} alt="Profile" className="max-[700px]:hidden" />
+              <p className="max-[700px]:text-[10px] max-[700px]:leading-[17px] max-[700px]:w-[70px] max-[700px]:text-center max-[700px]:flex max-[700px]:justify-center max-[700px]:items-center text-[1rem] leading-[1.5rem] font-normal">Profile</p>
+            </div>
+            <div className="py-[8px]">
+              <Image src={Line} alt="Line" className="min-[701px]:hidden h-full w-[3px] rounded-[10px]" />
+            </div>
+          </Link>
+
+
+          <div className="hover:bg-white-secondary active:bg-white-secondary max-[700px]:p-[6px] p-[12px] flex flex-row flex-warp gap-[10px] cursor-pointer" onClick={handleLogout} id="sidebar-menu">
             <Image src={logout} alt="Log out" className="max-[700px]:hidden" />
             <p className="max-[700px]:text-[10px] max-[700px]:leading-[17px] max-[700px]:w-[70px] max-[700px]:text-center max-[700px]:flex max-[700px]:justify-center max-[700px]:items-center text-[1rem] leading-[1.5rem] font-normal">Log out</p>
+          </div>
+
         </div>
-          </Link>
-      </div>
-      <div className="box-2 pl-[16px] max-[700px]:text-center max-[700px]:hidden">
-        <p>Â© 202X - Get That Job</p>
-      </div>
+        <div className="box-2 pl-[16px] max-[700px]:text-center max-[700px]:hidden">
+          <p>Â© 202X - Get That Job</p>
+        </div>
       </div>
     </div>
   );
