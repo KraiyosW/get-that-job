cdbf1917d597f54ba447bd9a955deebee16bcf3d
Bright <22whiskey.smd@gmail.com>
2023-04-25 16:42:57 +0700

    feat:fetching test

--- src/pages/api/findthatjob.js
+++ src/pages/api/findthatjob.js
@@ -1,31 +1,37 @@
-import nextConnect from 'next-connect'
-import { supabase } from '../../contexts/authentication'
-const handler = nextConnect()
-// export default async function FindthatJob(req, res) {
-//     try {
-//         const { rows } = await supabase.from('job_test').select('*').limit(10)
+import { createClient } from "@supabase/supabase-js";
 
-//         res.statusCode = 200
-//         res.json({ data: rows })
-//     } catch (error) {
-//         res.statusCode = 500
-//         res.json({ error: error.message })
-//     }
-// };
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-handler.get(async (req, res) => {
-    const { data, error } = await supabase
-        .from('job_test')
-        .find('*')
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
-    if (error) {
-        res.status(500).json({ data })
-    } else {
-        res.status(200).json({ message: 'Form data submitted successfully' })
+export default async function FindthatJob(req, res) {
+    try {
+        const rows = await supabase.from('job_test').select('*').limit(10)
+        res.statusCode = 200
+        res.json({ rows })
+    } catch (error) {
+        res.statusCode = 500
+        res.json({ error: error.message })
     }
-})
+};
 
-export default handler
+// import nextConnect from 'next-connect'
+// const handler = nextConnect()
+// handler.get(async (req, res) => {
+//     const { data, error } = await supabase
+//         .from('job_test')
+//         .find('*')
+
+//     if (error) {
+//         res.status(500).json({ data })
+//     } else {
+//         res.status(200).json({ message: 'Form data submitted successfully' })
+//     }
+// })
+
+// export default handler
+
 // try {
 //     const { method } = req;
 //     switch (method) {
