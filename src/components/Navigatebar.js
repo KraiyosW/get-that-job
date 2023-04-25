449c2c3363f465eb3987b4e798596805cc310b21
Mu Vonglao <muvonglao@gmail.com>
2023-04-25 17:00:43 +0700

    change link name in navbar

--- src/components/Navigatebar.js
+++ src/components/Navigatebar.js
@@ -5,7 +5,6 @@ import GTJ from "../image/gtj.svg";
 import Image from "next/image";
 import Link from "next/link";
 
-
 const Navigatebar = () => {
   return (
     <div className="w-full flex flex-row flex-wrap justify-between shadow-md relative z-10 max-[514px]:mt-5 max-[514px]:justify-center">
@@ -18,14 +17,22 @@ const Navigatebar = () => {
       </Link>
       <div className="nav-button flex flex-row flex-wrap max-[514px]:mr-0 max-[514px]:justify-center max-[767px]:mr-[2.5rem] max-[1060px]:mr-[5rem] min-[1061px]:mr-[10rem]">
         <Link href="/register">
-          <div className="group flex flex-row border-[1px] rounded-[16px] border-solid border-pink-primary w-[150px] my-[8px] h-[45px] justify-center gap-[7px] mr-2 min-[767px]:mr-[15px] hover:text-white hover:bg-pink-primary duration-[0.5s] active:opacity-[50%]" >
-            <Image alt="signup-icon" src={signup} className="group-hover:brightness-[250%] duration-[0.5s]" />
-            <button >SIGN UP</button>
+          <div className="group flex flex-row border-[1px] rounded-[16px] border-solid border-pink-primary w-[150px] my-[8px] h-[45px] justify-center gap-[7px] mr-2 min-[767px]:mr-[15px] hover:text-white hover:bg-pink-primary duration-[0.5s] active:opacity-[50%]">
+            <Image
+              alt="signup-icon"
+              src={signup}
+              className="group-hover:brightness-[250%] duration-[0.5s]"
+            />
+            <button>SIGN UP</button>
           </div>
         </Link>
-        <Link href="/Login">
+        <Link href="/login">
           <div className="group flex flex-row border-[1px] rounded-[16px] border-solid border-pink-primary w-[150px] my-[8px] h-[45px] justify-center gap-[7px] mr-2 min-[767px]:mr-[15px] hover:text-white hover:bg-pink-primary duration-[0.5s] active:opacity-[50%]">
-            <Image alt="login-icon" src={login} className="group-hover:brightness-[250%] duration-[0.5s]" />
+            <Image
+              alt="login-icon"
+              src={login}
+              className="group-hover:brightness-[250%] duration-[0.5s]"
+            />
             <button>LOGIN</button>
           </div>
         </Link>
