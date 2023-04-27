aefd281eb25558060fc9e47be94f4fb285c4c909
YokYok <patiparn.wong@gmail.com>
2023-04-27 12:08:18 +0700

    feat(findthatjob-filter): add filter-job UI

--- src/pages/find-that-job/index.js
+++ src/pages/find-that-job/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import SidebarProfessional from "@/components/SidebarProfessional";
-import Findthatjob from "@/components/professional-page/findthatjob";
+import Findthatjob from "@/components/professional-page/FindthatJob";
 
 const FindThatJob = () => {
   return (
     <>
       <div>
         <SidebarProfessional />
+        <FilterJob />
         <Findthatjob />
       </div>
     </>
--- src/pages/findthatjob.js
+++ src/pages/findthatjob.js
@@ -1,10 +1,13 @@
 import React from "react";
 import SidebarProfessional from "@/components/SidebarProfessional";
-import Findthatjob from "@/components/professional-page/findthatjob";
+import Findthatjob from "@/components/professional-page/FindthatJob";
+import FilterJob from "@/components/professional-page/FilterJob";
+
 const FindThatJob = () => {
   return (
     <div>
       <SidebarProfessional />
+      <FilterJob/>
       <Findthatjob />
     </div>
   );
