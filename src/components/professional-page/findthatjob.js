78ca066e9b62ae8bc8a1a61332e23a7ad3edb9e6
Bright <22whiskey.smd@gmail.com>
2023-05-02 11:16:55 +0700

    feat(findthatjob):filter fetching)

--- src/components/professional-page/findthatjob.js
+++ src/components/professional-page/findthatjob.js
@@ -8,30 +8,33 @@ import following from "../../image/following.png";
 import categorypic from "../../image/categorypic.png";
 import calendar from "../../image/calendar.png";
 import dollar from "../../image/dollar.png";
-import { createClient } from "@supabase/supabase-js";
 import Link from "next/link";
-
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 const Findthatjob = () => {
 
     const [job, setJob] = useState([]);
+    const [searchMessage, setSearchMessage] = useState("");
+    const [selectedOption, setSelectedOption] = useState("");
+    const [selectedJobType, setSelectedJobType] = useState("");
     const router = useRouter();
     const [isAuthenticated, setIsAuthenticated] = useState(false);
     // const [followStatus, setFollowStatus] = useState({});
     // const [applicationStatus, setApplicationStatus] = useState({});
-    const AllJob = async () => {
+
+    const getJobs = async () => {
         try {
-            const result = await axios.get('http://localhost:3000/api/findthatjob')
+            const params = new URLSearchParams();
+            params.append("job_title", searchMessage);
+            params.append("job_category", selectedOption);
+            params.append("job_type", selectedJobType);
+            const result = await axios.get(`http://localhost:3000/api/findthatjob?${params.toString()}`)
             setJob(result.data.job.data);
-        } catch {
+
+        } catch (error) {
             console.error();
         }
     };
-    
+
 
     // const result = await axios.get(
     //     'http://localhost:3000/api/findthatjob'
@@ -52,10 +55,6 @@ const Findthatjob = () => {
     //     });
     // };
 
-
-
-    const [jobList, setJobList] = useState([]);
-    const [searchMessage, setSearchMessage] = useState("");
     const [formData, setFormData] = useState({
         job_category: "",
         job_type: "",
@@ -63,9 +62,7 @@ const Findthatjob = () => {
         salary_max_range: "",
     });
 
-    // สร้าง state สำหรับเก็บข้อมูล option ที่เลือก
-    const [selectedOption, setSelectedOption] = useState("");
-    const [selectedJobType, setSelectedJobType] = useState("");
+
     // ฟังก์ชั่นสำหรับการเปลี่ยนค่าใน form
     const handleChange = (event) => {
         setFormData({
@@ -99,20 +96,20 @@ const Findthatjob = () => {
 
     useEffect(() => {
         const token = localStorage.getItem("sb:token"); // ใช้ localStorage ในการเก็บ token
-        setIsAuthenticated(!!token); 
-        AllJob();
-    }, [isAuthenticated]);
+        setIsAuthenticated(!!token);
+        getJobs({ searchMessage, selectedOption, selectedJobType });
+    }, [searchMessage, selectedOption, selectedJobType]);
 
     if (!isAuthenticated) {
-    return (<div className="max-w-full max-h-screen flex flex-col flex-warp items-center px-[50px] min-[768px]:px-[120px] ">
-    <h2 className="mt-[3rem] text-center text-pink-primary" id="heading2">!!! Please log in before accessing this page !!!</h2>
-    <Link href='/login' className="mt-[2rem] underline underline-offset-[10px] hover:text-pink-primary text-[2rem] ">Login page.....</Link>
-    </div>);
+        return (<div className="max-w-full max-h-screen flex flex-col flex-warp items-center px-[50px] min-[768px]:px-[120px] ">
+            <h2 className="mt-[3rem] text-center text-pink-primary" id="heading2">!!! Please log in before accessing this page !!!</h2>
+            <Link href='/login' className="mt-[2rem] underline underline-offset-[10px] hover:text-pink-primary text-[2rem] ">Login page.....</Link>
+        </div>);
 
-     
-  }
 
-    const filteredJobs = job.filter(item => item.job_title.toLowerCase().includes(searchMessage.toLowerCase()) || item.job_category.includes(selectedOption));
+    }
+
+    // const filteredJobs = job.filter(item => item.job_title.toLowerCase().includes(searchMessage.toLowerCase()) || item.job_category.includes(selectedOption));
 
 
 
@@ -222,9 +219,9 @@ const Findthatjob = () => {
 
 
                     <div className="flex flex-col flex-wrap w-full items-center">
-                        <h6 className="max-[700px]:text-center mb-4 mt-4">{filteredJobs.length} jobs for you</h6>
+                        <h6 className="max-[700px]:text-center mb-4 mt-4">{job.length} jobs for you</h6>
                         <div className="flex felx-row flex-wrap gap-[15px]">
-                            {filteredJobs.map((item, index) => {
+                            {job.map((item, index) => {
 
                                 return (
                                     <div
