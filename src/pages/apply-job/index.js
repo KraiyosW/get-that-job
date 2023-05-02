d787893f07e90148c9e4877ae9fce05cb1e11143
Mu Vonglao <muvonglao@gmail.com>
2023-05-02 14:59:01 +0700

    feat(job applications): init this feature

---
+++ src/pages/apply-job/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import SidebarProfessional from "@/components/SidebarProfessional";
+import EditJobPosting from "@/components/recruiter-page/EditJobPosting.js";
+
+const ApplyJob = () => {
+  return (
+    <>
+      <div>
+        <SidebarProfessional />
+      </div>
+    </>
+  );
+};
+
+export default ApplyJob;
