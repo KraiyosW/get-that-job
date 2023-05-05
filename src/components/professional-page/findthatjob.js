a152697c58525b4e0f073e06fdf7ac943537f174
Bright <22whiskey.smd@gmail.com>
2023-05-05 16:06:20 +0700

    checkout to other branch

--- src/components/professional-page/findthatjob.js
+++ src/components/professional-page/findthatjob.js
@@ -9,7 +9,6 @@ import categorypic from "../../image/categorypic.png";
 import calendar from "../../image/calendar.png";
 import dollar from "../../image/dollar.png";
 import Warning from "../Warning";
-import { createClient } from "@supabase/supabase-js";
 
 const Findthatjob = () => {
 
@@ -17,11 +16,11 @@ const Findthatjob = () => {
   const [searchMessage, setSearchMessage] = useState("");
   const [category, setCategory] = useState("");
   const [selectedJobType, setSelectedJobType] = useState("");
-  const [salaryMin, setSalaryMin] = useState("");
-  const [salaryMax, setSalaryMax] = useState("");
   const router = useRouter();
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [followStatus, setFollowStatus] = useState({});
+  const [sortAscending, setSortAscending] = useState(false);
+  const [sortDescending, setSortDescending] = useState(false);
 
   const getJobs = async () => {
     try {
@@ -32,6 +31,16 @@ const Findthatjob = () => {
     } catch (error) {
       console.log(error);
     }
+  };
+
+  const handleSortAscending = () => {
+    setSortAscending(true);
+    setSortDescending(false);
+  };
+
+  const handleSortDescending = () => {
+    setSortDescending(true);
+    setSortAscending(false);
   };
 
   const [formData, setFormData] = useState({
@@ -72,18 +81,11 @@ const Findthatjob = () => {
     return text;
   }
 
-  function handleSalaryMin(event) {
-    setSalaryMin(event.target.value);
-  }
-
-  function handleSalaryMax(event) {
-    setSalaryMax(event.target.value);
-  }
-
   useEffect(() => {
     const token = localStorage.getItem("sb:token"); // ใช้ localStorage ในการเก็บ token
     setIsAuthenticated(!!token);
     getJobs();
+
   }, [isAuthenticated, followStatus]);
   console.log(job);
 
@@ -157,53 +159,11 @@ const Findthatjob = () => {
     }
   });
 
-  // const filterJobs = job.filter((jobs) => {
-  //     if (category !== "Select or create a category" && searchMessage.toLowerCase() !== "" && selectedJobType !== "Select a type" && salaryMin !== "" && salaryMax !== "") {
-  //         return (
-  //             (jobs.job_category.includes(category) &&
-  //                 jobs.job_title.toLowerCase().includes(searchMessage) &&
-  //                 jobs.job_type.includes(selectedJobType) &&
-  //                 jobs.salary_min_range >= salaryMin &&
-  //                 jobs.salary_max_range <= salaryMax) ||
-  //             (jobs.job_category.includes(category) &&
-  //                 jobs.job_description.toLowerCase().includes(searchMessage) &&
-  //                 jobs.job_type.includes(selectedJobType) &&
-  //                 jobs.salary_min_range >= salaryMin &&
-  //                 jobs.salary_max_range <= salaryMax)
-  //         );
-  //     } else if (category !== "Select or create a category" && selectedJobType !== "Select a type" && salaryMin !== "" && salaryMax !== "") {
-  //         return jobs.job_category.includes(category) && jobs.job_type.includes(selectedJobType) && jobs.salary_min_range >= salaryMin && jobs.salary_max_range <= salaryMax;
-  //     } else if (searchMessage.toLowerCase() !== "" && selectedJobType !== "Select a type" && salaryMin !== "" && salaryMax !== "") {
-  //         return (
-  //             jobs.job_title.toLowerCase().includes(searchMessage) ||
-  //             jobs.job_description.toLowerCase().includes(searchMessage)) &&
-  //             jobs.job_type.includes(selectedJobType) &&
-  //             jobs.salary_min_range >= salaryMin &&
-  //             jobs.salary_max_range <= salaryMax;
-  //     } else if (category !== "Select or create a category" && salaryMin !== "" && salaryMax !== "") {
-  //         return jobs.job_category.includes(category) && jobs.salary_min_range >= salaryMin && jobs.salary_max_range <= salaryMax;
-  //     } else if (searchMessage.toLowerCase() !== "" && salaryMin !== "" && salaryMax !== "") {
-  //         return (
-  //             jobs.job_title.toLowerCase().includes(searchMessage) ||
-  //             jobs.job_description.toLowerCase().includes(searchMessage)) &&
-  //             jobs.salary_min_range >= salaryMin &&
-  //             jobs.salary_max_range <= salaryMax;
-  //     } else if (selectedJobType !== "Select a type" && salaryMin !== "" && salaryMax !== "") {
-  //         return jobs.job_type.includes(selectedJobType) && jobs.salary_min_range >= salaryMin && jobs.salary_max_range <= salaryMax;
-  //     } else if (category !== "Select or create a category") {
-  //         return jobs.job_category.includes(category);
-  //     } else if (searchMessage.toLowerCase() !== "") {
-  //         return (
-  //             jobs.job_title.toLowerCase().includes(searchMessage) ||
-  //             jobs.job_description.toLowerCase
-  //                 ().includes(searchMessage)
-  //         );
-  //     } else if (selectedJobType !== "Select a type") {
-  //         return jobs.job_type.includes(selectedJobType);
-  //     } else {
-  //         return jobs;
-  //     }
-  // });
+  if (sortAscending) {
+    filterJobs.sort((a, b) => a.salary_min_range - b.salary_min_range);
+  } else if (sortDescending) {
+    filterJobs.sort((a, b) => b.salary_min_range - a.salary_min_range);
+  }
 
   return (
     <main className="bg-[#F5F5F6] h-screen">
@@ -281,6 +241,7 @@ const Findthatjob = () => {
                     <option value="Part-Time">Part Time</option>
                   </select>
                 </div>
+
                 <div>
                   <p
                     className="text-[#616161] mb-[4px] tracking-[1.5px]"
@@ -289,41 +250,20 @@ const Findthatjob = () => {
                     SALARY RANGE
                   </p>
                   <div className="flex flex-row items-center max-[700px]:justify-center">
-                    <input
-                      className="text-[#616161] border-solid border border-[#F48FB1] rounded-[8px] gap-[8px] p-[8px] max-[767px]:w-[90px] w-[102px] h-[36px] mr-[8px]"
-                      name="salary_min_range"
-                      placeholder="min"
-                      type="text"
-                      id="input-range"
-                      value={salaryMin}
-                      onChange={handleSalaryMin}
-                    />
-                    <svg
-                      width="11"
-                      height="2"
-                      viewBox="0 0 11 2"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
+
+                    <button
+                      className="bg-white border-solid border border-[#F48FB1] rounded-[8px] px-[8px] mr-[15px] h-[36px]"
+                      onClick={handleSortAscending}
                     >
-                      <line
-                        x1="1"
-                        y1="1"
-                        x2="10"
-                        y2="1"
-                        stroke="#8E8E8E"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                      />
-                    </svg>
-                    <input
-                      className="text-[#616161] border-solid border border-[#F48FB1] rounded-[8px] gap-[8px] p-[8px] max-[767px]:w-[90px] w-[102px] h-[36px] ml-[8px]"
-                      name="salary_max_range"
-                      placeholder="max"
-                      type="text"
-                      id="input-range"
-                      value={salaryMax}
-                      onChange={handleSalaryMax}
-                    />
+                      Low to High
+                    </button>
+                    <button
+                      className="bg-white border-solid border border-[#F48FB1] rounded-[8px] px-[8px] h-[36px]"
+                      onClick={handleSortDescending}
+                    >
+                      High to Low
+                    </button>
+
                   </div>
                 </div>
               </div>
--- src/pages/find-that-job/index.js
+++ src/pages/find-that-job/index.js
@@ -1,12 +1,18 @@
 import React from "react";
 import SidebarProfessional from "@/components/SidebarProfessional";
 import Findthatjob from "@/components/professional-page/findthatjob";
+import { getServerSideProps as getServerSidePropsFindthatjob } from "./getServerSideProps";
 
-const FindThatJob = () => {
+export async function getServerSideProps(context) {
+  const findThatJobProps = await getServerSidePropsFindthatjob(context);
+  return { props: { ...findThatJobProps.props } };
+}
+
+const FindThatJob = (props) => {
   return (
     <div>
       <SidebarProfessional />
-      <Findthatjob />
+      <Findthatjob {...props} />
     </div>
   );
 };
