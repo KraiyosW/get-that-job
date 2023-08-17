dd0b9cc8d17f6e4cab090cf8b93ec8c4a32c01c0
KraiyosW <kraiyos.wanna@gmail.com>
2023-08-17 13:50:11 +0700

    fix : localstrage error

--- src/pages/create-new-job/index.js
+++ src/pages/create-new-job/index.js
@@ -1,18 +1,16 @@
-import React from "react";
+import React from 'react'
 import Head from "next/head";
-import SideBarRecruiter from "@/components/SidebarRecruiter";
-import CreateNewJob from "@/components/recruiter-page/CreateNewJob";
+import SideBarRecruiter from '@/components/SidebarRecruiter';
+import CreateNewJob from '@/components/recruiter-page/CreateNewJob';
 
 function createNewJob() {
-  const isLocalStorageAvailable =
-    typeof window !== "undefined" && window.localStorage;
   return (
     <>
-      <Head />
-      <SideBarRecruiter />
-      <CreateNewJob isLocalStorageAvailable={isLocalStorageAvailable} />
+    <Head/>
+    <SideBarRecruiter/>
+    <CreateNewJob/>
     </>
-  );
+  )
 }
 
-export default createNewJob;
+export default createNewJob
